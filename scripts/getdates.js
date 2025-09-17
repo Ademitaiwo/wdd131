045c1abd18ddd50dd9fcbffce80c774ced24ef58
Ademitaiwo <owolabitaiye80@gmail.com>
2025-09-17 10:30:05 +0100

    Fist puch for wdd131 week02

--- scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-
-  // Dynamically set current year
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-    // Dynamically set last modified date
-    document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
-
